dd9064f383e3e477afc5528c2a16d9e728071312
johnneyop <johnneyop@gmail.com>
2022-10-25 11:14:23 -0500

    Part 1 updates
    1. implemented rapid api shazam core api
    2. Update discover jsx
    3. Add div content to error & Loader jsx files

---
+++ src/redux/services/shazamCore.js
@@ -0,0 +1,20 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+
+export const shazamCoreApi = createApi({
+    reducerPath: 'shazamCoreApi',
+    baseQuery: fetchBaseQuery({
+        baseUrl: 'https://shazam-core.p.rapidapi.com/v1',
+        prepareHeaders: (headers) => {
+            headers.set('X-RapidAPI-Key', 'b0865e7443msh17ffbce305983f1p1fe7f5jsn316cd04f79df');
+
+            return headers;
+        },
+    }),
+    endpoints: (builder) => ({
+        getTopCharts: builder.query({ query: () => '/charts/world' }),
+    }),
+});
+
+export const {
+    useGetTopChartsQuery,
+} = shazamCoreApi;
